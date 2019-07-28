01c7c2aa757b8a302959d8509065d1784277fac2
satishjasthi <satish27may@gmail.com>
2019-07-28 16:59:56 +0530

    Add Image Normalization and standardization code

--- drshti_yantrikarana/config.py
+++ drshti_yantrikarana/config.py
@@ -11,12 +11,16 @@ db_username = 'root'
 db_password = 'password'
 db_name = 'drshit_yantrikarana'
 
-# data configs
+# data configs##########################################################################################################
 data_dir = Path('/Users/satishjasthi/Documents/Professional/ML/drshti_yantrikarana/data')
 hdf5_data = data_dir.joinpath(data_dir.parent, 'HDF5_data_files/Data.h5')
 tfRecord_data = data_dir.joinpath(data_dir.parent, 'TFRecords_data_files/Data.tfrecords')
 
 
-# data preprocessing
+# data preprocessing####################################################################################################
 resize_shape = (224,224)
+# mu and sigma must be tf.float64
+# TODO add code to calculate data_mu and data_sigma
+data_mu = ''
+data_sigma = ''
 channel_depth = 3
